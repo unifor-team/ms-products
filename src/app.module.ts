5a0576462aa1593a3ddde2fc4d5507dfc8bae910
Manelitu <emanuelwork1@gmail.com>
2024-11-26 18:07:24 -0300

    feat: criacao do guards de autenticacao

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { ProductController } from './controller/product.controller';
 import { ProductService } from './services/product.service';
 import { ProductRepository } from './repository/Product';
 import { PrismaService } from './core/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guard/auth.guard';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [],
   controllers: [ProductController],
-  providers: [ProductService, ProductRepository, PrismaService],
+  providers: [ProductService, ProductRepository, PrismaService, JwtService, {
+    provide: APP_GUARD,
+    useClass: AuthGuard,
+  }],
 })
 export class AppModule {}
--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { Products } from "@prisma/client";
 import { CreateProduct } from "src/dto/create-product";
+import { AuthGuard } from "src/guard/auth.guard";
 import { ProductService } from "src/services/product.service";
 
 @Controller("/api/products")
+@UseGuards(AuthGuard)
 export class ProductController {
     constructor(private service: ProductService) {}
 
